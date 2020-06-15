b212cb9efb72a380427d80da6a0a74e5b3ff23b1
swathysathian <66917847+swathysathian@users.noreply.github.com>
2020-06-15 12:01:30 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,18 @@
+function count1
+{
+  let s=1
+  while($s -gt 0)
+    if[[$count -eq $m]]
+    then
+      cho "Congratulations"
+      break
+    elif[[$count -gt $m]]
+      echo "Too High"
+    else
+      echo "Too Low"
+    fi
+ }
+
 read -p "Enter the no of files in current directory" count
 let m=$(ls -1 | wc -l)
-if[[$count -eq $m]]
-then
-  echo "Congratulations"
-elif[[$count -gt $m]]
-  echo "Too High"
-else
-  echo "Too Low"
-fi
+count1
